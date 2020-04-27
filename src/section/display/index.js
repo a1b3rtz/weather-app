fb93978cd6d266dbbaa05fb6d16a847b0ff90c25
YuhaoZhang-tirsky <tirsky@outlook.com>
2020-04-27 18:35:27 +1000

    weather detail layout

--- src/section/display/index.js
+++ src/section/display/index.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 import {
-  DisplayWrapper
+  DisplayWrapper,
+  Title,
+  Date,
+  WeatherIcon,
+  Data
 } from './style'
 
 class Display extends Component {
   render() {
     return (
-      <DisplayWrapper>display</DisplayWrapper>
+      <DisplayWrapper>
+        <Title>Weather</Title>
+        <Date>April 27th 2020</Date>
+        <WeatherIcon />
+        <Data>26°C</Data>
+        <Data>Melbourne</Data>
+        <Data className='detail'>Clear</Data>
+      </DisplayWrapper>
     )
   }
 }
--- src/section/display/style.js
+++ src/section/display/style.js
@@ -4,6 +4,43 @@ export const DisplayWrapper = styled.div`
   height: 700px;
   width: 650px;
   float: right;
-  background-color: red;
   border-radius: 0 10px 10px 0;
+  background-color: green;
+`
+
+export const Title = styled.h1`
+  font-size: 35px;
+  color: #ffffff;
+  font-weight: bold;
+  text-align: center;
+  margin-top: 60px;
+  :after {
+    content: '____';
+    display: block;
+  }
+`
+
+export const Date = styled.div`
+  font-size: 20px;
+  color: #ffffff;
+  text-align: center;
+  margin-top: 30px;
+`
+
+export const WeatherIcon = styled.div`
+  background-color: red;
+  width: 120px;
+  height: 120px;
+  margin: 0 auto;
+  margin-top: 60px;
+`
+
+export const Data = styled.div`
+  font-size: 30px;
+  color: #ffffff;
+  text-align: center;
+  margin-top: 30px;
+  &.detail {
+    font-size: 20px;
+  }
 `
